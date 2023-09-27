93e38bd43dd9565ff771a2e17df285f0482f0c52
BK <benjamin@wonder4studio.com>
2023-09-27 11:34:19 +0800

    feat: 1. 修正 slf4j pom 設定 2. mongo sample project.

---
+++ spring-boot-mongo/src/main/java/com/bk/sample/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.bk.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class SampleController {
+
+  @GetMapping("sample")
+  public String sampleGet() {
+    return "hello";
+  }
+
+}
---
+++ spring-boot-mongo/src/test/java/com/bk/sample/test/SampleTest.java
@@ -0,0 +1,10 @@
+package com.bk.sample.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class SampleTest {
+
+}
